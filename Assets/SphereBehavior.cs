2a3d33e43be7dc314774bd4428e6e46b6779f1f2
Kokenishii <ki575@nyu.edu>
2021-08-10 21:32:12 -0400

    Add New Project
    - Added in new assets
    - scenes

---
+++ Assets/SphereBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBehavior : MonoBehaviour
+{
+    public float xForce=0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(new Vector3(xForce, 0, 0), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += new Vector3(0,-0.1f,0);
+        
+
+    }
+
+    // will be called once the gameobject hits something with a collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.localScale *= 1.05f;
+    }
+   
+}
